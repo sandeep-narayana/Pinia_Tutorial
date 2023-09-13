5355b5c189d9f1c07f715e84d973795fed3dccf4
Sandeep Rawat <132456591+sandeep-narayana@users.noreply.github.com>
2023-09-13 12:28:14 +0530

    added heading and global css

--- pinia_tutorial/src/App.vue
+++ pinia_tutorial/src/App.vue
@@ -1,10 +1,16 @@
 <template>
-  <h1>App.vue</h1>
+  <main>
+
+    <!-- Heading -->
+    <header>
+      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/1c/Pinialogo.svg/1200px-Pinialogo.svg.png" alt="pinia logo">
+      <h1>Pinia Task</h1>
+    </header>
+  </main>
 </template>
 
 <script>
-
 export default {
-  name: 'App',
-}
+  name: "App",
+};
 </script>
--- pinia_tutorial/src/main.js
+++ pinia_tutorial/src/main.js
@@ -1,4 +1,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import './assets/main.css'; // Import your main.css file
 
 createApp(App).mount('#app')
